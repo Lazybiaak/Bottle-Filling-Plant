040b2f5885f329475bb3c85a779a46c6b49bceb0
lazybiaak <mansubiku@gmail.com>
2024-05-28 04:35:55 -0400

    Camera Worked

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+import cv2
+import time as time
+vid = cv2.VideoCapture(2)
+while(True):
+	#print ("Hello World")
+	ret, frame = vid.read()
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+vid.release()
+
+cv2.destroyAllWindows()
